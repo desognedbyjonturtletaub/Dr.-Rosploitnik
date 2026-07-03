056becbe1ae179f4ec63b185607aeb626c7bc9ee
lelogint <lelogint@gmail.com>
2026-07-03 19:56:17 +1200

    Formatting folders, functions moved to secondary folder

---
+++ Modules/Data/Anims.lua
@@ -0,0 +1,14 @@
+return {
+	flyIdle = {
+		Self = nil,
+		Id = "rbxassetid://77529400769588"
+	},
+	flyMove = {
+		Self = nil,
+		Id = "rbxassetid://132105268936736"
+	},
+	boost = {
+		Self = nil,
+		Id = "rbxassetid://123393210843929"
+	},
+}
